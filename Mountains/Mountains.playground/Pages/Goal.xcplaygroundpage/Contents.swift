003548384e5f16a6e949b366a8d1aac74782dac0
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-01 07:54:47 -0500

    Initial commit

---
+++ Mountains/Mountains.playground/Pages/Goal.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: # Your goal
+//: ![goal](scenario-three.png "Your goal")
+/*:
+ ## Notes:
+ * the image is exactly 300 pixels wide by 300 pixels high
+ */
+//: ## Template code
+//: The following two statements are required to make the playground run. Please do not remove.
+import Cocoa
+import PlaygroundSupport
+//: ## Reminder
+//: To see the visual output, be sure to show the Assistant Editor and choose the Timeline option.
+//:
+//: ![assistanteditor](timeline.png "Timeline")
+//: ## Your code starts here
+// Create a new canvas
+let canvas = Canvas(width: 300, height: 300)
+
+// Draw some lines
+canvas.defaultLineWidth = 1
+canvas.drawLine(fromX: 100, fromY: 0, toX: 100, toY: 50)
+canvas.drawLine(fromX: 150, fromY: 0, toX: 150, toY: 100)
+canvas.drawLine(fromX: 200, fromY: 0, toX: 200, toY: 50)
+
+//: ## Template code
+//: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
+PlaygroundPage.current.liveView = canvas.imageView
